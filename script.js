f62202669fa0eacdfa501ce092a5700f9a45dd91
boy-son <branden.wheat@yahoo.com>
2022-12-22 12:14:21 -0500

    added a workaround for uppercase. still not functional

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword();
 
-var UCArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var UCArray = [];
 var LCArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var NumArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var CharArray = ['^', '$', '#', '@', '!', '%', '&', '*', '(', ')'];
+
 
 var resArray = [];
 var USArray = [];
@@ -16,6 +17,12 @@ var num = confirm ("Would you like numbers in your password?")
 var uppercase = confirm ("Would you like to use uppercase letters in your password?")
 var lowercase = confirm ("Would you like to add lowercase letters in your password?")
 var characters = confirm ("Would you like to use special characters in your password?")
+
+var toUpper = function (x) {
+return x.ToUpperCase();  
+}
+
+CaseChange = LCArray.map(toUpper)
 
 if (num) {
 resArray = resArray.concat(NumArray);  
