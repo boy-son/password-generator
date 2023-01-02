ac1c5f9c9e60a05774dcbe064e524127c7a41791
boy-son <branden.wheat@yahoo.com>
2023-01-02 17:44:34 -0500

    made more changes, button still does not work.

--- script.js
+++ script.js
@@ -6,45 +6,54 @@ var LCArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 var NumArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var CharArray = ['^', '$', '#', '@', '!', '%', '&', '*', '(', ')'];
 
-
-var resArray = [];
-var USArray = [];
-
-var numCharacter = prompt ("How many characters do you want between 8 & 128?");
-var num = confirm ("Would you like numbers in your password?")
-var uppercase = confirm ("Would you like to use uppercase letters in your password?")
-var lowercase = confirm ("Would you like to add lowercase letters in your password?")
-var characters = confirm ("Would you like to use special characters in your password?")
-
+var charcount = prompt("How many characters long would you like your password to be? \n8-128");
+var num = confirm("Would you like numbers in your password?")
+var uppercase = confirm("Would you like to use uppercase letters in your password?")
+var lowercase = confirm("Would you like to add lowercase letters in your password?")
+var characters = confirm("Would you like to use special characters in your password?")
 var toUpper = function (x) {
-return x.ToUpperCase();  
+  return x.ToUpperCase();
 }
-
-CaseChange = LCArray.map(toUpper);
+var CaseChange = LCArray.map(toUpper);
 
 function generatePassword(); {
+  var resArray = [];
+  var USArray = "";
+  if (charcount < 8 || charcount > 128) {
+    alert("Your password must be between 8 and 128 characters!")
+    generatePassword()
+  }
+  else {
+    if (num = true) {
+      resArray = resArray.concat(NumArray);
+    }
 
-if (num) {
-resArray = resArray.concat(NumArray);  
+    if (uppercase = true) {
+      resArray = resArray.concat(LCArray);
+      CaseChange = true
+    }
+
+    if (lowercase = true) {
+      resArray = resArray.concat(LCArray);
+    }
+
+    if (characters = true) {
+      resArray = resArray.concat(CharArray);
+    }
+    if (resArray.length === 0) {
+      alert("You must have at least one character type in your password");
+      writePassword()
+    } else {
+      for (var i = 0; i < charcount; i++) {
+        USArray.push(resArray[Math.floor[Math.random() * resArray.length]]);
+        USArray += resArray[random]
+      }
+    }
+    return USArray.join("");
+  }
 }
 
-if (uppercase) {
-resArray = resArray.concat(UCArray);
 
-}
-
-if (lowercase) {
-resArray = resArray.concat(LCArray);  
-}
-if (characters) {
-resArray = resArray.concat(CharArray);  
-}
-
-for (var i = 0; i < numCharacter; i++) {
-USArray.push (resArray[Math.floor[Math.random() * resArray.length]]);  
-} 
-return USArray.join("")
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -56,3 +65,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
